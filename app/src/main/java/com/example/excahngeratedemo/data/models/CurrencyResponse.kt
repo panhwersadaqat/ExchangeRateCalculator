7bd3d46eea166e0c85c18150d39eca4bd3b27665
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2023-03-07 15:51:51 +0400

    bug fix

--- app/src/main/java/com/example/excahngeratedemo/data/models/CurrencyResponse.kt
+++ app/src/main/java/com/example/excahngeratedemo/data/models/CurrencyResponse.kt
@@ -13,14 +13,14 @@ data class CurrencyResponse(
     val documentation: String,
     @SerializedName("terms_of_use")
     val terms_of_use: String,
-    @SerializedName("time_last_update_unix")
+/*    @SerializedName("time_last_update_unix")
     val time_last_update_unix: Int,
     @SerializedName("time_next_update_unix")
     val time_next_update_unix: String,
     @SerializedName("time_next_update_utc")
     val time_next_update_utc: Int,
     @SerializedName("time_eol_unix")
-    val time_eol_unix: Int,
+    val time_eol_unix: Int,*/
     @SerializedName("base_code")
     val base_code: String,
     @SerializedName("rates")
